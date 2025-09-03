8e2ab3c082b66afe957a741d2a7fba2b413cbcd0
SANDESHSSANSHI <117011942+SANDESHSSANSHI@users.noreply.github.com>
2025-09-03 20:28:01 +0530

    Update cdbConnection.jsx

--- A-migration-frontend-A/src/components/cdbConnection.jsx
+++ A-migration-frontend-A/src/components/cdbConnection.jsx
@@ -1,11 +1,17 @@
-// cdbConnection.jsx
 import PouchDB from 'pouchdb-browser';
-
-const couchdbUrl = process.env.COUCHDB_URL;
+const couchdbUrl = process.env.COUCHDB_URL; 
 const dbName = "transaction";
-
-const db = new PouchDB(`${couchdbUrl)/${dbName}`, {
-  skip_setup: false,
-});
-
+if (!couchdbUrl) {
+  throw new Error("REACT_APP_COUCHDB_URL is not defined in .env");
+}
+const db = new PouchDB(
+  `https://${couchdbUrl.replace(/\/$/, '')}/${dbName}`,
+  {
+    skip_setup: false
+  }
+);
+// Optional: test connection
+db.info()
+  .then(info => console.log("DB info:", info))
+  .catch(err => console.error("DB connection error:", err));
 export default db;
